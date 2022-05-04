8006492f3c4cb45c1c6d66e7005d3947354fa3a2
Haziq Naeem <haziqnaeem36@gmail.com>
2022-05-04 10:49:49 -0400

    distributor table

---
+++ api/models/distributor.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from api.models.db import db
+
+class Distributor(db.Model):
+    __tablename__ = 'distributor'
+    id = db.Column(db.Integer, primary_key=True)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transactions.id'), nullable=False)
+    name = db.Column(db.String(100))
+
+    def __init__(self, transaction_id, name):
+        self.transaction_id = transaction_id
+        self.name = name
+    
+    def __repr__(self):
+        return f"name {self.name}"
