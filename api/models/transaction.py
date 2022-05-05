a0b0aa12f9670d02abb9af93b8a258e17d236033
BootsStribling <williamstriblingenterprises@gmail.com>
2022-05-05 09:12:11 -0500

    fix transaction model

--- api/models/transaction.py
+++ api/models/transaction.py
@@ -1,29 +1,28 @@
 from datetime import datetime
+from pickle import FALSE
 from api.models.db import db
 
 class Transaction(db.Model):
   __tablename__='transactions'
   id = db.Column(db.Integer, primary_key=True)
   date = db.Column(db.String(100))
-  sellers_name = db.Column(db.String(100), nullable=False)
-  buyers_name = db.Column(db.String(100), nullable=False)
   sale_type = db.Column(db.String(100), nullable=False)
   transaction_total = db.Column(db.Float, nullable = False)
-  location_id = db.Column(db.String(100))
   community_id = db.Column(db.Integer, nullable=False)
+  manager = db.Column(db.String(1), nullable=FALSE)
   department_id = db.Column(db.Integer, nullable=False)
   country_id = db.Column(db.Integer, nullable=False)
   distributor = db.Column(db.Integer, nullable=False)
   
 
-  def __init__(self, sellers_name, buyers_name, location_id, transaction_total, date, sale_type, community_id, department_id, country_id, distributor):
+  def __init__(self, transaction_total, date, sale_type, community_id, manager, department_id, country_id, distributor):
         self.date = date
         self.sellers_name = sellers_name
         self.buyers_name = buyers_name
         self.sale_type = sale_type
         self.transaction_total = transaction_total
-        self.location_id = location_id
         self.community_id = community_id
+        self.manager = manager
         self.department_id = department_id
         self.country_id = country_id
         self.distributor = distributor
